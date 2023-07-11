5bf06f6dc018f3d5a10f15c5e06d4ff5d07bc13a
ZeptoTF <ZeptoTF@users.noreply.github.com>
2023-07-11 12:35:08 +0200

    Added more genes
    Added more genes.

--- Assets/Scripts/CreratureManager.cs
+++ Assets/Scripts/CreratureManager.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class CreratureManager : MonoBehaviour
 {
     private float[] genes;
+    [SerializeField] Rigidbody2D rb;
     [SerializeField] Color color;
+    [SerializeField] float mass;
 
     // Start is called before the first frame update
     void Start()
     {
         genes = GetComponent<GenesManager>().genesArray;
+        CreratureSize();
         CreatureColor();
-        CreratureSizeXY();
     }
 
     // Update is called once per frame
@@ -21,14 +23,14 @@ public class CreratureManager : MonoBehaviour
         
     }
 
-    private void CreatureColor()
+    private void CreratureSize()
     {
-        color = new Color(genes[0], genes[1], genes[2]);
-        this.GetComponent<SpriteRenderer>().color = color;
+        transform.localScale *= genes[0];
     }
 
-    private void CreratureSizeXY()
+    private void CreatureColor()
     {
-        transform.localScale *= genes[3];
+        color = new Color(genes[2], genes[3], genes[4]);
+        this.GetComponent<SpriteRenderer>().color = color;
     }
 }
--- Assets/Scripts/GenesManager.cs
+++ Assets/Scripts/GenesManager.cs
@@ -15,28 +15,119 @@ public class GenesManager : MonoBehaviour
     {   // Initialize each set of genes with random values
         genesList.Clear();
 
-        genesList.AddRange(RndColor(128, 255));
-        genesList.AddRange(RndSize(0.5f, 1.25f));
+        genesList.Add(RndSize(0.5f, 1.25f));
+        genesList.Add(RndShieldPercentage(0, 1));
+        genesList.Add(RndColorR(128, 255));
+        genesList.Add(RndColorG(128, 255));
+        genesList.Add(RndColorB(128, 255));
+        genesList.Add(AgeingFactor(0, 5));
+        genesList.Add(MaturityAge(0, 200));
+        genesList.Add(OldnessAge(0, 500));
+        genesList.Add(MaxExtraEnergyStoragePercentage(0, 1));
+        genesList.AddRange(SmellingEnergy(0, 1, 0, 5));
+        genesList.AddRange(ProducingEnergy(0, 1, 0, 5));
         // Other genes will be added
 
         // Store in an array to (possibly) improve performance when getting values 
         genesArray = genesList.ToArray();
     }
 
-    private float[] RndColor(float lowLimit, float highLimit)
-    {   // Generates the color
-        float r = UnityEngine.Random.Range(lowLimit, highLimit)/255;
-        float g = UnityEngine.Random.Range(lowLimit, highLimit)/255;
-        float b = UnityEngine.Random.Range(lowLimit, highLimit)/255;
-        float[] rgb = {r, g, b};
-        return rgb;
-    }
-
-    private float[] RndSize(float lowLimit, float highLimit)
-    {
-        // Sets the size of the creature
-        float scale = UnityEngine.Random.Range(lowLimit, highLimit);
-        float[] size = {scale};
+    private float RndSize(float lowLimit, float highLimit)
+    {   // Sets the size of the creature
+        float size = UnityEngine.Random.Range(lowLimit, highLimit);
         return size;
     }
+
+    private float RndShieldPercentage(float lowLimit, float highLimit)
+    {   // Sets the percentage of damage reduction
+        float shielding = UnityEngine.Random.Range(lowLimit, highLimit);
+        return shielding;
+    }
+
+    private float RndColorR(float lowLimit, float highLimit)
+    {   // Generates the colorR
+        float r = UnityEngine.Random.Range(lowLimit, highLimit) / 255;
+        return r;
+    }
+
+    private float RndColorG(float lowLimit, float highLimit)
+    {   // Generates the colorG
+        float g = UnityEngine.Random.Range(lowLimit, highLimit) / 255;
+        return g;
+    }
+
+    private float RndColorB(float lowLimit, float highLimit)
+    {   // Generates the colorB
+        float b = UnityEngine.Random.Range(lowLimit, highLimit) / 255;
+        return b;
+    }
+
+    private float GeneMutationsProbabilty(float lowLimit, float highLimit)
+    {
+        float geneMutationProbabilty = UnityEngine.Random.Range(lowLimit, highLimit);
+        return geneMutationProbabilty;
+    }
+
+    private float BrainMutationsProbabilty(float lowLimit, float highLimit)
+    {
+        float brainMutationProbabilty = UnityEngine.Random.Range(lowLimit, highLimit);
+        return brainMutationProbabilty;
+    }
+
+    private float AgeingFactor(float lowLimit, float highLimit)
+    {   // Sets the speed at which the creature ages
+        float ageingFactor = UnityEngine.Random.Range(lowLimit, highLimit);
+        return ageingFactor;
+    }
+
+    private float MaturityAge(float lowLimit, float highLimit)
+    {
+        float maturityAge = UnityEngine.Random.Range(lowLimit, highLimit);
+        return maturityAge;
+    }
+
+    private float OldnessAge(float lowLimit, float highLimit)
+    {
+        float OldnessAge = UnityEngine.Random.Range(lowLimit, highLimit);
+        return OldnessAge;
+    }
+
+    private float MaxExtraEnergyStoragePercentage(float lowLimit, float highLimit)
+    {
+        float maxExtraEnergyStorage = UnityEngine.Random.Range(lowLimit, highLimit);
+        return maxExtraEnergyStorage;
+    }
+
+    private float[] SmellingEnergy(int lowLImitCSE, int highLimitCSE, float lowLimitESST, float highLimitESST)
+    {
+        float canSmellEnergy = UnityEngine.Random.Range(lowLImitCSE, highLimitCSE);
+        float energySmellSensitivityThreshold;
+        if (canSmellEnergy == 1)
+        {
+            energySmellSensitivityThreshold = UnityEngine.Random.Range(lowLimitESST, highLimitESST);
+        }
+        else
+        {
+            energySmellSensitivityThreshold = -1;
+        }
+        float[] smellingEnergy = { canSmellEnergy, energySmellSensitivityThreshold };
+        return smellingEnergy;
+    }
+
+    private float[] ProducingEnergy(int lowLimitCPE, int highLimitCPE, float lowLimitEPEP, float highLimitEPEP)
+    {
+        float canProduceEnergy = UnityEngine.Random.Range(lowLimitCPE, highLimitCPE);
+        float energyProductionEfficiencyPercentage;
+        if (canProduceEnergy == 1)
+        {
+            energyProductionEfficiencyPercentage = UnityEngine.Random.Range(lowLimitEPEP, highLimitEPEP);
+        }
+        else
+        {
+            energyProductionEfficiencyPercentage = -1;
+        }
+        float[] producingEnergy = { canProduceEnergy, energyProductionEfficiencyPercentage };
+        return producingEnergy;
+    }
+
 }
